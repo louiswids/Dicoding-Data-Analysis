7f7d212080c7180989504b82d2139a3cb1f74683
Louis Widi Anandaputra <137313724+louiswids@users.noreply.github.com>
2023-10-16 15:10:27 +0700

    Update dashboard.py into Bahasa Indonesia

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,62 +2,63 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pydeck as pdk
 
 # Load Data
 data = pd.read_csv("main_data.csv") 
 
 # Title
-st.title("Air Quality Dashboard")
+st.title("Dashboard Kualitas Udara")
 
 # Sidebar
 st.sidebar.header("Project by Louis Widi Anandaputra\nFilter Data")
-year = st.sidebar.selectbox("Select Year", data['year'].unique())
-month = st.sidebar.selectbox("Select Month", data['month'].unique())
-parameter = st.sidebar.selectbox("Select Parameter", ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO', 'O3'])
-selected_stations = st.sidebar.multiselect("Select Stations", data['station'].unique())
+year = st.sidebar.selectbox("Pilih Tahun", data['year'].unique())
+month = st.sidebar.selectbox("Pilih Bulan", data['month'].unique())
+parameter = st.sidebar.selectbox("Pilih Parameter", ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO', 'O3'])
+selected_stations = st.sidebar.multiselect("Pilih Stasiun Perekaman", data['station'].unique())
 
 # Filter data
 filtered_data = data[(data['year'] == year) & (data['month'] == month) & (data['station'].isin(selected_stations))]
 
 # Time Series Analysis
-st.header("Time Series Analysis")
-st.write("Time Series Analysis for Air Quality Parameters")
+st.header("Analisis Tren")
+st.write("Analisis Tren Parameter Kualitas Udara")
 fig, ax = plt.subplots(figsize=(10, 5))
 sns.lineplot(x='hour', y=parameter, data=filtered_data, ax=ax)
 st.pyplot(fig)
 
 # Air Quality Metrics
-st.header("Air Quality Metrics")
-st.write("Select an air quality parameter to view its chart:")
+st.header("Metrik Kualitas Udara")
+st.write("Pilih Metrik Kualitas Udara untuk Melihat Grafik:")
 selected_param = st.selectbox("Parameter", ['PM2.5', 'PM10', 'SO2', 'NO2', 'CO', 'O3'])
 fig, ax = plt.subplots(figsize=(10, 5))
 sns.lineplot(x='hour', y=selected_param, data=filtered_data, ax=ax)
 st.pyplot(fig)
 
 # Temperature and Pressure
-st.header("Temperature and Pressure Analysis")
+st.header("Analisis Suhu dan Tekanan")
 fig, ax = plt.subplots(2,figsize=(10, 5))
 sns.lineplot(x='hour', y='TEMP', data=filtered_data, ax=ax[0], label="Temperature")
 sns.lineplot(x='hour', y='PRES', data=filtered_data, ax=ax[1], label="Pressure")
 st.pyplot(fig)
 
 # Dew Point and Rain
-st.header("Dew Point and Rain Analysis")
+st.header("Analisis Titik Embun dan Hujan")
 fig, ax = plt.subplots(2,figsize=(10, 5))
 sns.lineplot(x='hour', y='DEWP', data=filtered_data, ax=ax[0], label="Dew Point")
 sns.lineplot(x='hour', y='RAIN', data=filtered_data, ax=ax[1], label="Rain")
 st.pyplot(fig)
 
 # Wind Information
-st.header("Wind Information Analysis")
+st.header("Analisis Informasi Udara")
 fig, ax = plt.subplots(figsize=(10, 5))
 sns.lineplot(x='hour', y='WSPM', data=filtered_data, ax=ax, label="Wind Speed")
 st.pyplot(fig)
 
 # Acquire Information about SO2, NO2, CO, and O3 levels
-st.header("Acquire Information about SO2, NO2, CO, and O3 Levels")
-parameter_to_acquire = st.selectbox("Select Parameter", ['SO2', 'NO2', 'CO', 'O3'])
-st.subheader(f"Information about {parameter_to_acquire} levels:")
+st.header("Informasi Mengenai Tingkat SO2, NO2, CO, dan O3")
+parameter_to_acquire = st.selectbox("Pilih Parameter", ['SO2', 'NO2', 'CO', 'O3'])
+st.subheader(f"Informasi mengenai tingkat {parameter_to_acquire} :")
 if parameter_to_acquire in data.columns:
     st.write(f"Mean {parameter_to_acquire} Level: {filtered_data[parameter_to_acquire].mean()}")
     st.write(f"Median {parameter_to_acquire} Level: {filtered_data[parameter_to_acquire].median()}")
